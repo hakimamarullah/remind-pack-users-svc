8e794de14131c8b6e39e4bff02b19e0fbc7d3c4a
hakimamarullah <hakimamarullah@gmail.com>
2025-07-11 19:10:23 +0700

    Add spring cloud loadbalancer

--- src/main/java/com/starline/users/feign/WhatsAppProxySvc.java
+++ src/main/java/com/starline/users/feign/WhatsAppProxySvc.java
@@ -6,8 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 
-@FeignClient(name = "whatsapp-proxy",
-        url = "${whatsapp.proxy.url:http://localhost:4000}",
+@FeignClient(name = "${srv.feign.names.whatsapp:whatsapp}",
         configuration = {FeignLogBasicConfig.class}
 )
 public interface WhatsAppProxySvc {
