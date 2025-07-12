a7009f01e9b22f8ae388a773d1247a68295bfa9a
hakimamarullah <hakimamarullah@gmail.com>
2025-07-12 18:58:15 +0700

    Setup otel AsyncConfig

--- src/main/java/com/starline/users/config/AsyncConfig.java
+++ src/main/java/com/starline/users/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package com.starline.users.config;
 
+import io.opentelemetry.context.Context;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
@@ -7,17 +8,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.Executor;
+
 @Configuration
 @Slf4j
 public class AsyncConfig implements AsyncConfigurer {
 
     @Bean
-    public ThreadPoolTaskExecutor taskExecutor() {
+    public Executor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(4);
         executor.setMaxPoolSize(5);
         executor.setQueueCapacity(500);
-        return executor;
+        return Context.taskWrapping(executor);
     }
 
     @Override
